cba1ce10be216957c27b3cc31e888ccb24a0c102
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-09-24 22:14:40 -0400

    Working Data to Screen
    Putting data to screen

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const app = Vue.createApp ({
     data() {
         return {
-            date: null
+            date: "",
+            img: null,
+            media: null,
+            explanation: null,
+            title: null,
+            imgDate: null
         }
     },
     methods: {
         dateSearch(date) {
             axios
             .get("https://api.nasa.gov/planetary/apod?api_key=2jbed0ziekmkhL6uZBoIkbPdaSVSdL1H2LGBxJHI&date=" + date)
-            .then(response => (console.log(response), console.log(date)))
+            .then(response => (console.log(response), this.img = response.data.url, this.media = response.data.media_type, this.title = response.data.title, this.imgDate = response.data.date, this.explanation = response.data.explanation))
             .catch(e => (console.log(e)))
         }
     }
