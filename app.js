399f83b42379efc425d4d282eaf1d070690e94ad
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-09-30 18:12:33 -0400

    Styling
    Loading spinner

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ const app = Vue.createApp ({
             media: null,
             explanation: null,
             title: null,
-            imgDate: null
+            imgDate: null,
+            spinner: false
         }
     },
     methods: {
         dateSearch(date) {
             axios
             .get("https://api.nasa.gov/planetary/apod?api_key=2jbed0ziekmkhL6uZBoIkbPdaSVSdL1H2LGBxJHI&date=" + date)
-            .then(response => (console.log(response), this.img = response.data.url, this.media = response.data.media_type, this.title = response.data.title, this.imgDate = response.data.date, this.explanation = response.data.explanation))
+            .then(response => (console.log(response), this.img = response.data.url, this.media = response.data.media_type, this.title = response.data.title, this.imgDate = response.data.date, this.explanation = response.data.explanation, this.spinner = false))
             .catch(e => (console.log(e)))
+        },
+        spinnerToggle(spinner) {
+            this.spinner = true
         }
     }
 })
